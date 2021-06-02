53a636b4147347ecf5c658aed2db88bbc0d4428b
Trung <ductrung4263@gmail.com>
2021-06-02 18:02:08 +0700

    form react-render l

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
-
+import React,{useState} from 'react';
 function App() {
+  const [status, setStatus] = useState("Show")
+  const list = <ul style={status === "Show" ? {display: "block"} : {display: "none"}}>
+    <li>Pham Duc Trung</li>
+    <li>Pham Duc Trinh</li>
+    <li>Pham Duc Trong</li>
+  </ul>
+  const clickMe =()=>{
+    {status === "Show"? setStatus("Hide") : setStatus("Show")}
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <button onClick={clickMe}>{status === "Show" ? "Show" : "Hide"}</button>
+     {list}
     </div>
   );
 }
